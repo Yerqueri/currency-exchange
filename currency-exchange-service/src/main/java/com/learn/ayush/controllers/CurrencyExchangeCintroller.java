93665dcb945075cad8fc42a4adf148a769ad06fd
Yadav <ayush.yadav@cgi.com>
2018-09-25 17:20:54 +0530

    added jpa dependency to currency-exchange-service

---
+++ currency-exchange-service/src/main/java/com/learn/ayush/controllers/CurrencyExchangeCintroller.java
@@ -0,0 +1,25 @@
+package com.learn.ayush.controllers;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learn.ayush.models.ExchangeValue;
+
+@RestController
+public class CurrencyExchangeCintroller {
+	
+	@Autowired
+	private Environment env;
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from,@PathVariable String to){
+		ExchangeValue eval =new ExchangeValue(1L,from,to,BigDecimal.valueOf(0.65));
+		eval.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		return eval;
+	}
+}
